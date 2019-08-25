43afbaf77f7f02a6c319a09795793da1ab6a9b7a
李梦尧 <16637039498@163.com>
2019-08-25 13:17:58 +0800

    提交

---
+++ src/com/jp/HelloGit.java
@@ -0,0 +1,14 @@
+package com.jp;
+
+/**
+ * @author lmy
+ * @create 2019-08-25 13:05
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("test Git");
+
+    }
+}
