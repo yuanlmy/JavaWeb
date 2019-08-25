ef4333256ff9160440204cb2dc30579bdbdebb94
李梦尧 <16637039498@163.com>
2019-08-25 14:03:31 +0800

    提交

---
+++ mavemdemo/src/main/java/com/keystone/mavemdemo/MavemdemoApplication.java
@@ -0,0 +1,13 @@
+package com.keystone.mavemdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavemdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavemdemoApplication.class, args);
+    }
+
+}
---
+++ mavemdemo/src/test/java/com/keystone/mavemdemo/MavemdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.keystone.mavemdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MavemdemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
